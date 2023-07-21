b37a564b41def526134446410d1cd9a2cb088801
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-07-21 22:01:24 +0530

    Setup of smtp using nodemailer is done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service')
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -10,6 +12,12 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log("Server started at PORT : ", PORT);
+        sendBasicEmail(
+            "admin@kenil.in",
+            "suzalkachhadiya2912@gmail.com",
+            "Hello from Prachi",
+            "I am Prachi , i love you"
+        )
     });
 }
 
