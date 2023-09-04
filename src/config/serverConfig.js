63e887ce9af3d9628123a921aeaca79f666f0dd2
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-09-04 19:55:09 +0530

    Update route according to API Gateway

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,7 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT : process.env.PORT,
-    EMAIL_ID : process.env.EMAIL_ID,
-    EMAIL_PASSWORD : process.env.EMAIL_PASSWORD
+    PORT: process.env.PORT,
+    EMAIL_ID: process.env.EMAIL_ID,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
 // const { sendBasicEmail } = require('./services/email-service')
 const TicketController = require('./controllers/ticket-controller');
 const jobs = require('./utils/job');
+const EmailService = require('./services/email-service');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.post('/api/v1/tickets' , TicketController.create);
+    app.post('reminderservice/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    await subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
         console.log("Server started at PORT : ", PORT);
-        jobs();
-        // sendBasicEmail(
-        //     "admin@kenil.in",
-        //     "suzalkachhadiya2912@gmail.com",
-        //     "Hello from Prachi",
-        //     "I am Prachi , i love you"
-        // )
+        // jobs();
     });
 }
 
